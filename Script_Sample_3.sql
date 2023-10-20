108d91dfd2adabe92abf209268709a8e59e005b9
Sumet Jutajan <sumet.jutha@gmail.com>
2023-10-20 09:48:57 +0700

    Update Script_Sample_3.sql

--- Script_Sample_3.sql
+++ Script_Sample_3.sql
@@ -8,3 +8,15 @@ FROM Customer;
 
 SELECT COUNT(DISTINCT City)
 FROM Customer;
+
+SELECT City
+FROM Customer
+WHERE Country = 'USA';
+
+SELECT City
+FROM Customer
+WHERE Country = 'Brazil';
+
+SELECT City
+FROM Customer
+WHERE Country IN ('Brazil','Canada');
