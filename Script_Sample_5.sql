210d3baf6c935013d0964b06a9b23d0fa0d2d02a
Sumet Jutajan <sumet.jutha@gmail.com>
2023-10-21 21:27:45 +0700

    Update Script_Sample_5.sql

--- Script_Sample_5.sql
+++ Script_Sample_5.sql
@@ -5,3 +5,40 @@ IN (SELECT CustomerId
 	WHERE Country = 'Germany'
 );
 
+UPDATE Customer
+SET SupportRepId = (SupportRepId * 1.2)
+WHERE SupportRepId IN (SELECT SupportRepId FROM Customer WHERE Country = 'Germany');
+
+UPDATE Customer
+SET SupportRepId = SupportRepId * 1.2
+WHERE Country = 'Germany';
+
+SELECT *
+FROM Customer WHERE CustomerId 
+IN (SELECT CustomerId
+	FROM Customer
+	WHERE Country = 'Germany'
+);
+
+SELECT * FROM customer;
+
+/*############################################## */
+
+UPDATE Customer
+SET SupportRepId = (SupportRepId * 1.2)
+WHERE SupportRepId IN (SELECT MIN(SupportRepId) FROM Customer);
+
+SELECT * FROM customer;
+
+
+
+
+
+
+
+
+
+
+
+
+
