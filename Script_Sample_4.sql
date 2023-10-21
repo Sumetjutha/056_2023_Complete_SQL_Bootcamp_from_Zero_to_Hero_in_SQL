595bd506af84be3f79dc7b723dcbd8d47232043b
Sumet Jutajan <sumet.jutha@gmail.com>
2023-10-21 20:04:17 +0700

    Update Script_Sample_4.sql

--- Script_Sample_4.sql
+++ Script_Sample_4.sql
@@ -3,3 +3,19 @@ SELECT * FROM Customer;
 CREATE VIEW Customer_view AS
 SELECT FirstName,LastName FROM Customer;
 
+SELECT *
+FROM Customer_view;
+
+UPDATE Customer_view
+SET LastName = 'Diaz'
+WHERE FirstName = 'Emma';
+
+UPDATE Customer
+SET LastName = 'Diaz'
+WHERE FirstName = 'Emma';
+
+SELECT *
+FROM Customer_view;
+
+DROP VIEW Customer_view;
+
